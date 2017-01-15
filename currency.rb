ca486237049e3b151369ec1a281d5dd391a9af5f
cmfeeney <colinmfeeney@gmail.com>
2017-01-15 14:14:16 -0500

    Addition test and raise

--- currency.rb
+++ currency.rb
@@ -8,4 +8,9 @@ class Currency
   def ==(other)
     @amount == other.amount && @code == other.code
   end
+
+  def +(other)
+      raise RuntimeError.new('Different Currency Code Error') unless @code == other.code
+        Currency.new(amount: @amount + other.amount, code: @code)
+  end
 end
--- currency_test.rb
+++ currency_test.rb
@@ -14,5 +14,20 @@ class CurrencyTest < Minitest::Test
 
     c = Currency.new(amount: 34, code: 'EUR')
     refute a == c
+
+    d = Currency.new(amount: 30, code: 'USD')
+    refute a == d
+  end
+
+  def test_add_currencies
+    a = Currency.new(amount: 34, code: 'USD')
+    b = Currency.new(amount: 30, code: 'USD')
+    c = Currency.new(amount: 64, code: 'USD')
+    assert a + b == c
+
+    d = Currency.new(amount: 34, code: 'EUR')
+    assert_raises RuntimeError do
+      a + d
+    end
   end
 end
