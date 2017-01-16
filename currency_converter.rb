5f6cd4a12ab31b94758808fa7754af76d6d1650f
cmfeeney <colinmfeeney@gmail.com>
2017-01-15 20:16:47 -0500

    Add Unknown Currency Code Error and Tests

--- currency_converter.rb
+++ currency_converter.rb
@@ -10,17 +10,13 @@ class CurrencyConverter
   end
 
   def convert(a,b)
-    if a.code == b
-      Currency.new(amount: a.amount, code: a.code)
-    elsif curr_rates[a.code] > curr_rates[b]
-      Currency.new(amount: a.amount * curr_rates[b], code: b)
-    else
-      Currency.new(amount: a.amount / curr_rates[a.code], code: b)
-    end
+    raise RuntimeError.new('Unknown Currency Code Error') unless curr_rates[a.code] != nil && curr_rates[b] != nil
+      if a.code == b
+        Currency.new(amount: a.amount, code: a.code)
+      elsif curr_rates[a.code] > curr_rates[b]
+        Currency.new(amount: a.amount * curr_rates[b], code: b)
+      else
+        Currency.new(amount: a.amount / curr_rates[a.code], code: b)
+      end
   end
 end
-
-a = CurrencyConverter.new(USD: 1.0, EUR: 0.943585)
-b = Currency.new(amount: 1, code: 'EUR')
-c = a.convert(b, 'USD')
-p c
--- currency_converter_test.rb
+++ currency_converter_test.rb
@@ -15,9 +15,18 @@ class CurrencyConverterTest < Minitest::Test
     d = Currency.new(amount: 0.943585, code: 'EUR')
     e = Currency.new(amount: 0.830174, code: 'GBP')
     f = Currency.new(amount: 1, code: 'USD')
+    g = Currency.new(amount: 1 / 0.943585, code: 'USD')
+    h = Currency.new(amount: 1, code: 'FFF')
     assert a.convert(b, 'USD') == b
     assert a.convert(c, 'EUR') == c
     assert a.convert(b, 'EUR') == d
     assert a.convert(f, 'GBP') == e
+    assert a.convert(c, 'USD') == g
+    assert_raises RuntimeError do
+      a.convert(h, 'USD')
+    end
+    assert_raises RuntimeError do
+      a.convert(b, 'FFF')
+    end
   end
 end
